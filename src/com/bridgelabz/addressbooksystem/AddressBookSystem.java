08d0129eee90e93a2039a97964e3d55c48071d48
sujit <sujitbokade2014@gmail.com>
2022-07-20 16:07:06 +0530

    Add Multiple Contacts

--- src/com/bridgelabz/addressbooksystem/AddressBookSystem.java
+++ src/com/bridgelabz/addressbooksystem/AddressBookSystem.java
@@ -1,20 +1,37 @@
 package com.bridgelabz.addressbooksystem;
 
+import java.util.Scanner;
+
 public class AddressBookSystem {
     public static void main(String[] args) {
-           System.out.println("Welcome to Address Book System Program!");
-
-            AddressBook addressBook=new AddressBook();
-            Contact contact=new Contact();
-
-            addressBook.addContact();
-            System.out.print(addressBook.showInfo());
-
-             addressBook.editContact();
-             System.out.print(addressBook.showInfo());
-
-             addressBook.deleteContact();
-             System.out.print(addressBook.showInfo());
-
-        }
+        System.out.println("Welcome to Address Book Program");
+        AddressBook addressbook = new AddressBook();
+        Scanner sc = new Scanner(System.in);
+        boolean exit = true;
+        do {
+            System.out.println("Enter 1 to Add Contact");
+            System.out.println("Enter 2 to Edit Contact");
+            System.out.println("Enter 3 to Delete");
+            System.out.println("Enter 4 to Exit");
+            int option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    addressbook.addContact();
+                    break;
+                case 2:
+                    addressbook.editContact();
+                    break;
+                case 3:
+                    addressbook.showInfo();
+                    break;
+                case 4:
+                    addressbook.deleteContact();
+                    break;
+                case 5:
+                    exit = false;
+                default:
+                    break;
+            }
+        }while (exit);
+    }
 }
